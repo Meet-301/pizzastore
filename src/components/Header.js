88c4ae6963832c42b78d053ecf3825583144d167
Meet-301 <pujarameet301@gmail.com>
2024-08-09 15:06:05 +0000

    Aug 9, 2024, 8:36 PM

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#explore-pizzas">Explore Pizzas</a></li>
+                    <li><a href="#about-us">About Us</a></li>
+                    <li><a href="#contact-us">Contact Us</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
