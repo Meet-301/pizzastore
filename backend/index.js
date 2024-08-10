81796dcfc21d2f33d41c057663da9f3334f61438
Meet-301 <pujarameet301@gmail.com>
2024-08-10 11:44:41 +0000

    Aug 10, 2024, 5:14 PM

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 
+app.get("/",)
+
 // Route to handle updating items with image upload
 app.put('/items/:id', upload.single('image'), (req, res) => {
   const itemId = req.params.id;
