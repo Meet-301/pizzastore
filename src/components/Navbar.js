8b4c0d1c38be57edd75466aed40def041d20ccf3
Meet-301 <pujarameet301@gmail.com>
2024-08-10 10:43:05 +0000

    Aug 10, 2024, 4:13 PM

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function MyNavbar() {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand as={Link} to="/">Pizza Store</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/" style={"fon"}>Butter Crust</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
