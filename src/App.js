c5d1856b08230dbd9b7d95e3e0e8ea2a70cf970c
Meet-301 <pujarameet301@gmail.com>
2024-08-10 02:15:12 +0000

    Aug 10, 2024, 7:45 AM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import DisplayAllItems from './components/DisplayAllItems';
+import AddNewItem from './components/AddNewItem';
+import UpdateItem from './components/UpdateItem';
+import DeleteItem from './components/DeleteItem';
+import Contact from './components/Contact';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/items" component={DisplayAllItems} />
+                    <Route path="/add" component={AddNewItem} />
+                    <Route path="/update/:id" component={UpdateItem} />
+                    <Route path="/delete/:id" component={DeleteItem} />
+                    <Route path="/contact" component={Contact} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
