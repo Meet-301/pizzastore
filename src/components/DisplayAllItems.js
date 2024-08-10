b1e2b61c9c679df14beb317fbe3c0b29275b6f69
Meet-301 <pujarameet301@gmail.com>
2024-08-10 02:13:03 +0000

    Aug 10, 2024, 7:43 AM

--- src/components/DisplayAllItems.js
+++ src/components/DisplayAllItems.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const DisplayAllItems = () => {
-  return (
-    <div>DisplayAllItems</div>
-  )
+function DisplayAllItems() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/items')
+            .then(response => {
+                setItems(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error fetching the items!', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>All Items</h1>
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>{item.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default DisplayAllItems
+export default DisplayAllItems;
