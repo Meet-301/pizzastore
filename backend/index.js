ead79e7d9ece8098a5b9c7a16550de1d91a470c8
Meet-301 <pujarameet301@gmail.com>
2024-08-10 12:53:37 +0000

    Aug 10, 2024, 6:23 PM

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 // Setup storage for Multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/images');
+    cb(null, path.join(__dirname, '../public/images'));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
