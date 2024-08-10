53948cfb4881878a14a1fce0803d4858f743963e
Meet-301 <pujarameet301@gmail.com>
2024-08-10 03:24:59 +0000

    Aug 10, 2024, 8:54 AM

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Carousel } from 'react-bootstrap';
 import '../styles/Home.css';
-import pizza1 from "../images/pizza1.jpg"
+import pizza1 from "../images/pizza1.avif"
 
 function Home() {
     return (
