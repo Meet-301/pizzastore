e6ced96225ff1f4b492a715bb7b235f91533a000
Meet-301 <pujarameet301@gmail.com>
2024-08-11 03:14:01 +0000

    Aug 11, 2024, 8:44 AM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Footer from './Footer'; // Import the Footer component
-import './App.css'; // Import the CSS file for styling
+import Home from './components/Home';
+import DisplayAllItems from './components/DisplayAllItems';
+import MyItems from './components/MyItems';
+import AddItem from "./components/AddItem";
+import UpdateItem from './components/UpdateItem';
+import DeleteItem from './components/DeleteItem';
+import MyNavbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
-  return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <div className="flex-fill">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-        <Footer /> {/* Place the Footer component here */}
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <MyNavbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/items" element={<DisplayAllItems />} />
+                    <Route path="/myitems" element={<MyItems />} />
+                    <Route path="/add" element={<AddItem />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/update/:id" element={<UpdateItem />} />
+                    <Route path="/delete/:id" element={<DeleteItem />} />
+                </Routes>
+            </div>
+            <Footer />
+        </Router>
+        
+    );
 }
 
 export default App;
