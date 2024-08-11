a814f655303ad1fb7a0541eb43442e84261d94c9
Meet-301 <pujarameet301@gmail.com>
2024-08-11 03:04:58 +0000

    Aug 11, 2024, 8:34 AM

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
 import DisplayAllItems from './components/DisplayAllItems';
 import MyItems from './components/MyItems';
 import AddItem from "./components/AddItem";
 import UpdateItem from './components/UpdateItem';
 import DeleteItem from './components/DeleteItem';
-import Contact from './components/Contact';
 import MyNavbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -18,13 +16,11 @@ function App() {
                 <MyNavbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
                     <Route path="/items" element={<DisplayAllItems />} />
                     <Route path="/myitems" element={<MyItems />} />
                     <Route path="/additem" element={<AddItem />} />
                     <Route path="/update/:id" element={<UpdateItem />} />
                     <Route path="/delete/:id" element={<DeleteItem />} />
-                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </div>
             <Footer />
